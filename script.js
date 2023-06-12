9ad7404976b26778c0d272ebb2e0197ed472f348
silver-shadow <107355428+silver-shadow@users.noreply.github.com>
2023-06-12 21:00:00 +0530

    Add the code to createGrid function

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 const container = document.querySelector(".container");
 
-for(let i = 0; i < 16; i++)
+function createGrid()
 {
-    let row = document.createElement("div");
-    row.setAttribute("style","display :flex;");
-    for(let j = 0; j < 16; j++)
+    for(let i = 0; i < 16; i++)
     {
-        let div = document.createElement("div");
-        div.classList.add("square");
-        //div.textContent = `${j}`;
-        div.setAttribute("style","padding : 10px; margin: 2px; border-style: solid")
-        row.appendChild(div);
+        let row = document.createElement("div");
+        row.setAttribute("style","display :flex;");
+        for(let j = 0; j < 16; j++)
+        {
+            let div = document.createElement("div");
+            div.classList.add("square");
+            //div.textContent = `${j}`;
+            div.setAttribute("style","padding : 10px; margin: 2px; border-style: solid")
+            row.appendChild(div);
+        }
+        container.appendChild(row);
     }
-    container.appendChild(row);
 }

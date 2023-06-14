73738aaf27814472f7a7c5a01a0c3a908931a617
silver-shadow <107355428+silver-shadow@users.noreply.github.com>
2023-06-15 01:07:09 +0530

    Add a button to prompt user Button prompts user for number of squares to be generated in the grid Set fixed width and height for grid container

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 const container = document.querySelector(".container");
 
-function createGrid()
+function createGrid(n)
 {
-    for(let i = 0; i < 16; i++)
+    let contSize = 900;
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+    for(let i = 0; i < n; i++)
     {
         let row = document.createElement("div");
         row.setAttribute("style","display :flex;");
-        for(let j = 0; j < 16; j++)
+        for(let j = 0; j < n; j++)
         {
             let div = document.createElement("div");
+            div.style.width = contSize/n + 'px';
+            div.style.height = contSize/n + 'px';
             div.classList.add("square");
             //div.textContent = `${j}`;
-            div.setAttribute("style","padding : 10px; margin: 2px; border-style: solid")
+            //div.setAttribute("style",`padding: ${960/n}px; margin: 0px; border-style: solid`);
             row.appendChild(div);
         }
         container.appendChild(row);
@@ -31,4 +37,20 @@ function createGrid()
     });
 }
 
-createGrid()
+function promptUser()
+{
+    let num = parseInt(prompt("Please enter size of grid to be created(1 to 100)"));
+    while(isNaN(num) || num<1 || num>100)
+    {
+        num = prompt("Invalid input. Please try again\nEnter size of grid to be created(1 to 100)");
+    }
+    createGrid(num);
+}
+
+const button = document.querySelector("button");
+button.addEventListener("click",() =>
+{
+    promptUser();
+});
+
+//promptUser()
